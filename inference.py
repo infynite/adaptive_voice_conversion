524f712fc91e793f97eac1a29e85fb0bef94fc9a
infynite <44844809+infynite@users.noreply.github.com>
2020-09-29 10:02:03 +0330

    Add raw output

--- inference.py
+++ inference.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     parser.add_argument('-source', '-s', help='source wav path')
     parser.add_argument('-target', '-t', help='target wav path')
     parser.add_argument('-output', '-o', help='output wav path')
-    parser.add_argument('-routput', '-o', help='raw mel output path')
+    parser.add_argument('-routput', '-r', help='raw mel output path')
     parser.add_argument('-sample_rate', '-sr', help='sample rate', default=24000, type=int)
     args = parser.parse_args()
     # load config file 
